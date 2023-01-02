c29925c7558b584169c433895bcbbb6d3a958ba5
Dustin Lee <leedustin@live.com>
2023-01-02 16:12:42 -0500

    test

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ const cTable = require('console.table');
 const mysql = require('mysql2');
 // require dotenv pkg and configure as early as possible
 require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'company_db'
+  });
