f8a68a4365730b0e0eaa3aa38c0341ad5831c1fd
Dustin Lee <leedustin@live.com>
2023-01-04 05:52:40 -0500

    added connectionto sql server

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO departments (department)
+VALUES  ('Sales'),
+        ('Engineering'),
+        ('Finance'),
+        ('Legal');
+
+INSERT INTO roles (titles, salary, department_id)
+VALUES  ('Sales'),
+        ('Engineering'),
+        ('Finance'),
+        ('Legal');
