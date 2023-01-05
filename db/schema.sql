6e31ba2458d3ae6b832f0f18b9ca5662c78cc182
Dustin Lee <leedustin@live.com>
2023-01-05 15:32:43 -0500

    added roles table seeds and query

--- db/schema.sql
+++ db/schema.sql
@@ -3,17 +3,20 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     department VARCHAR(30) NOT NULL
 );
-SELECT * FROM department;
+SELECT * FROM departments;
 
 CREATE TABLE roles (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL(M, 2) NOT NULL,
     department_id INT,
+    salary DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
 );
 
 SELECT * FROM roles;
@@ -22,7 +25,10 @@ CREATE TABLE employee (
     id INT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL AUTO_INCREMENT,
-    manager_id INT NULL
+    role_id INT,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
 );
 SELECT * FROM employee;
